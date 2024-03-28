9d0f486fb8d0be4f28af2572047483f0da88985b
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-03-28 01:57:02 +0100

    DELETED tests

--- test/outputs.tf
+++
@@ -1,7 +0,0 @@
-# Resources outputs
-
-// Output the Register VM ID
-output "register_instance_id" {
-  description = "The ID of the Register VM"
-  value = openstack_compute_instance_v2.register_vm.id
-}
